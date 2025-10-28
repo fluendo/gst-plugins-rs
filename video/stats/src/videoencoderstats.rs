83bf65f34cf00e74eaf3ccd57ec56e864ed3a4c3
Diego Nieto <dnieto@fluendo.com>
2025-10-28 16:56:38 +0100

    videoencoderstats: add current buffers seens metric

--- video/stats/src/videoencoderstats.rs
+++ video/stats/src/videoencoderstats.rs
@@ -32,6 +32,7 @@ pub struct VideoEncoderStats {
     pub num_bytes: u64,
     pub time_last_buffers: VecDeque<Instant>,
     pub max_buffers_inside: usize,
+    pub current_buffers_inside_seen: usize,
     pub num_buffers_first_output: i32,
     pub total_processing_time: Duration,
     pub threads_utime: u64,
@@ -52,6 +53,7 @@ impl Default for VideoEncoderStats {
             num_buffers: 0,
             time_last_buffers: VecDeque::<Instant>::new(),
             max_buffers_inside: 0,
+            current_buffers_inside_seen: 0,
             num_buffers_first_output: -1,
             total_processing_time: Duration::ZERO,
             threads_utime: 0,
@@ -70,7 +72,8 @@ impl VideoEncoderStats {
         if self.time_last_buffers.len() > self.max_buffers_inside {
             self.max_buffers_inside = self.time_last_buffers.len();
         }
-        gst::log!(CAT, "Current buffers lenght {}", self.time_last_buffers.len());
+        self.current_buffers_inside_seen = self.time_last_buffers.len();
+        gst::log!(CAT, "Current buffers length {}", self.current_buffers_inside_seen);
     }
 
     pub fn buffer_out(&mut self) {
@@ -114,6 +117,11 @@ impl fmt::Display for VideoEncoderStats {
             f,
             "Max. Buffers inside: {}",
             self.max_buffers_inside
+        )?;
+        writeln!(
+            f,
+            "Current buffers inside: {}",
+            self.current_buffers_inside_seen
         )?;
         writeln!(
             f,
